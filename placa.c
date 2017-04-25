6b5ea4d1a9630b9113a9ee44915d3eda5988bd4e
jrojas12 <jeroka.96@gmail.com>
2017-04-25 12:10:20 -0500

    sentencia if, arreglada ?

--- placa.c
+++ placa.c
@@ -4,13 +4,21 @@ void establece_condiciones_iniciales(int i,int f,float t,placa p, int type ){
   /* for open i conditions type is 0 */
   /* for periodic i conditions type is 1 */
   /* for stacked i conditions type is 2 */
-  if(type==0)
-  for ( i = 0; i < Array_largo; i++ ) {
+  #if(type == 0)
+      for ( i = 0; i < Array_largo; i++ ) {
     for ( f = 0; f < Array_ancho; f++ ) {
       p.shield[ i ][ f ] = t;
-   }
+      }
   }
-
+  #elif(type == 1)
+  
+    printf("\nNumber is Positive");
+  #else
+       printf("\nNumber is Negative");
+  
+  #endif
+  
+  
 
 
 
